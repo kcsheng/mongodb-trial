dba547788a7b58c2b42b06f50004a9e51ef54760
kcsheng <kacysheng@mail.com>
2021-06-24 22:29:35 +1000

    Set up mongodb connection with mongoose

---
+++ index.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+mongoose
+  .connect("mongodb://localhost/playground", {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+  })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch((error) => console.error("Could not connect to MongoDB...", error));
