dd0d3fe50ff7722e6c8e57f6eaf497be5f2822df
kcsheng <kacysheng@mail.com>
2021-06-29 11:24:22 +1000

    Set up query fucntion

--- index.js
+++ index.js
@@ -14,17 +14,39 @@ const courseSchema = new mongoose.Schema({
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
 });
-
+const Course = mongoose.model("Course", courseSchema); // Course blueprint - model(class)
 async function createCourse() {
-  const Course = mongoose.model("Course", courseSchema); // Course blueprint - model(class)
   const course = new Course({
     // A course instance is a document in the db.
     name: "Angular Course",
     author: "Mosh",
-    tags: ["angular", "frontend"],
+    tags: ["Angular", "frontend"],
+    isPublished: true,
   });
 
   const result = await course.save(); // This method returns a promise.
   console.log(result);
 }
-createCourse();
+async function getCourses() {
+  // eq (equal)
+  // ne (not equal to)
+  // gt (greater than)
+  // gte (greater than or equal to)
+  // lt (less than)
+  // lte (less than or equal to)
+  // in
+  // nin (not in)
+  const courses = await Course
+    // .find({
+    //   author: "Mosh",
+    //   isPublished: true,
+    // })
+    // .find({ price: { $gte: 10, $lte: 20 } })
+    .find({ price: { $in: [10, 20, 30] } })
+    .limit(10)
+    .sort({ name: 1 })
+    .select({ name: 1, tags: 1 });
+  // Property to be returned
+  console.log(courses);
+}
+getCourses();
