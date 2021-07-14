739a8565bda18ea4671c2502279ea01090eca7fd
kcsheng <kacysheng@mail.com>
2021-07-15 09:15:55 +1000

    Implement try catch block to catch validation errors

--- index.js
+++ index.js
@@ -8,24 +8,27 @@ mongoose
   .catch((error) => console.error("Could not connect to MongoDB...", error));
 
 const courseSchema = new mongoose.Schema({
-  name: String,
+  name: { type: String, required: true },
   author: String,
   tags: [String],
   date: { type: Date, default: Date.now },
   isPublished: Boolean,
 });
-const Course = mongoose.model("Course", courseSchema); // Course blueprint - model(class)
+const Course = mongoose.model("Course", courseSchema);
 async function createCourse() {
   const course = new Course({
-    // A course instance is a document in the db.
-    name: "Angular Course",
+    // name: "Angular Course",
     author: "Mosh",
     tags: ["Angular", "frontend"],
     isPublished: true,
   });
-
-  const result = await course.save(); // This method returns a promise.
-  console.log(result);
+  try {
+    // Use try and catch block to catch any validation errors
+    const result = await course.save();
+    console.log(result);
+  } catch (ex) {
+    console.log(ex.message);
+  }
 }
 async function getCourses() {
   const pageNumber = 2;
@@ -42,18 +45,4 @@ async function getCourses() {
   console.log(courses);
 }
 
-async function updateCourse(id) {
-  const course = await Course.findByIdAndUpdate(id, {
-    $set: {
-      author: "Jason",
-      isPublished: false,
-    },
-  });
-  console.log(result);
-}
-
-async function removeCourse(id) {
-  const result = await Course.deleteOne({ _id: id });
-  console.log(result);
-}
-removeCourse("60da6ccc06447078f9174b31");
+createCourse();
